baf4804d5ad9bc73a201b8027ef5fd79b3167f53
ArpitaHalvi <arpitahalvi@gmail.com>
2025-04-30 16:41:48 +0530

    Modified

--- client/src/pages/Vendors.jsx
+++ client/src/pages/Vendors.jsx
@@ -1,7 +1,23 @@
 import ContactUs from "./ContactUs";
 
 export default function Vendors() {
-  const sellers = [{}, {}, {}];
+  const sellers = [
+    {
+      text: "Some Shop",
+      imgUrl:
+        "https://res.cloudinary.com/dgkv2gft7/image/upload/v1746011141/seller3-2_tbdlyy.jpg",
+    },
+    {
+      text: "The Book House",
+      imgUrl:
+        "https://res.cloudinary.com/dgkv2gft7/image/upload/v1746011144/seller1-2_s60sk0.jpg",
+    },
+    {
+      text: "Akash Pustak Sadan",
+      imgUrl:
+        "https://res.cloudinary.com/dgkv2gft7/image/upload/v1746011149/seller2-2_emwc37.jpg",
+    },
+  ];
   return (
     <section className="vendors-section">
       <div className="become-seller">
@@ -11,14 +27,19 @@ export default function Vendors() {
         </p>
       </div>
       <div className="sellers-testimonies">
-        <h2>Hear from Our Sellers</h2>
+        <h2>Shop That Trust Sellers</h2>
         <div className="sellers">
           {sellers.map((seller, index) => {
-            return <div key={index} className="seller"></div>;
+            return (
+              <div key={index} className="seller">
+                <img src={seller.imgUrl} alt="Seller Photo" />
+                <h6 className="img-desc">{seller.text}</h6>
+              </div>
+            );
           })}
         </div>
       </div>
-      <h2 className="contact-heading">Become a Seller - Contact Us Today!</h2>
+      {/* <h2 className="contact-heading">Become a Seller - Contact Us Today!</h2> */}
       <ContactUs />
     </section>
   );
